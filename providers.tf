647753a79d96faa85e6bc5d711f886e28b32677b
LadyMarg <adeogunmargaret@gmail.com>
2025-06-16 03:38:34 +0000

    update

--- providers.tf
+++ providers.tf
@@ -21,7 +21,7 @@ provider "http" {}
 #backend
 terraform {
   backend "s3" {
-    region = "us-east-1"
+    region = "us-west-2"
     workspace_key_prefix = "microservices"
     key    = "terraform.tfstate"
     bucket = "margterraform"
